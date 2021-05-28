2c9de704bf76ad427f18bb39d7839f6210cda682
Matthew Zimmer <the.matthew.zimmer@gmail.com>
2021-05-28 17:58:41 -0400

    project initialized

---
+++ middle/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 95,
+      functions: 95,
+      lines: 95,
+      statements: 95
+    }
+  },
+  testRegex: 'test/.*\\.test\\.ts$'
+};
